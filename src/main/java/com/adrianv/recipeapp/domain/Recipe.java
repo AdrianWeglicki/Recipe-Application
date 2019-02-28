903b95a0ff74e3504324395c68fa6b0ac29e7e31
Adrian <adrianw@gmail.com>
2019-02-28 12:06:27 +0000

    completing relationships

--- src/main/java/com/adrianv/recipeapp/domain/Recipe.java
+++ src/main/java/com/adrianv/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.adrianv.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -17,6 +18,9 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -100,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
